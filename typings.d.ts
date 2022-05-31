9b4f3facd4053bcc5d8e370ea18c4c00f6877b19
ataubaldiyev <ataubaldiyev@beeline.kz>
2022-05-31 15:03:10 +0600

    Added typescript config and postcss config

---
+++ typings.d.ts
@@ -0,0 +1,8 @@
+declare module '*.scss' {
+    const content: { readonly [className: string]: string }
+    export default content
+}
+
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'

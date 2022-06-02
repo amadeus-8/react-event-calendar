5e2de198a83098c1fdec4084517566d31b9ddd86
ataubaldiyev <ataubaldiyev@beeline.kz>
2022-06-02 10:18:49 +0600

    Added dayjs config

--- src/redux/slices/calendarConfigSlice.ts
+++ src/redux/slices/calendarConfigSlice.ts
@@ -1,6 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
 import dayjs from 'dayjs'
+import ru from 'dayjs/locale/ru'
+import badMutable from 'dayjs/plugin/badMutable'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
 import { RootState } from '../reducers/rootReducer'
+
+// Config for mutable dayjs Object, and russian localization
+dayjs.locale(ru)
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(badMutable)
+dayjs.extend(customParseFormat)
+dayjs.tz.setDefault('Asia/Almaty')
 
 type ViewType = 'day' | 'week' | 'month'
 

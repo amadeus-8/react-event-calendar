1c0a5c2d8e6749f19c9c77c9305407528ede4be8
ataubaldiyev <ataubaldiyev@beeline.kz>
2022-06-02 10:26:16 +0600

    Calendar container completed

--- src/pages/calendar/components/calendarContainer/CalendarContainer.tsx
+++ src/pages/calendar/components/calendarContainer/CalendarContainer.tsx
@@ -2,6 +2,14 @@ import React, { FC } from 'react'
 
 import styles from './CalendarContainer.module.scss'
 
+import { CalendarHeader } from '../calendarHeader/CalendarHeader'
+import { CalendarBody } from '../calendarBody/CalendarBody'
+
 export const CalendarContainer: FC = () => {
-    return <div />
+    return (
+        <div className={styles.container}>
+            <CalendarHeader />
+            <CalendarBody />
+        </div>
+    )
 }
